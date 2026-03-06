c419efed1ba32769f691e30b1411493ac517fb1a
ISupportYou08 <yourspport08@gmail.com>
2026-03-06 15:40:05 +0800

    Add files via upload

---
+++ Add Activity 01 – Hello World Program/Activity01_HelloWorldProgram.java
@@ -0,0 +1,8 @@
+public class Activity01_HelloWorldProgram {
+    public static void main(String[]args){
+        System.out.println("Hello World!");
+        System.out.println("This is my first time!");
+        System.out.println("Look Ma! I'm Coding!");
+        
+    }
+}
